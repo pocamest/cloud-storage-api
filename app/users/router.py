587ff9c43d0f2b9e85d3699c15f045a691637d48
Rinat <pocamest@yandex.ru>
2026-01-18 00:08:45 +0300

    refactor: introduce fastapi constants for status codes (#4)

--- app/users/router.py
+++ app/users/router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
 
 from app.auth.dependencies import CurrentUserDep
 from app.users.dependencies import UserServiceDep
@@ -8,12 +8,12 @@ from app.users.schemas import UserCreate, UserRead
 router = APIRouter(prefix="/users", tags=["users"])
 
 
-@router.post("/", response_model=UserRead, status_code=201)
+@router.post("/", response_model=UserRead, status_code=status.HTTP_201_CREATED)
 async def create_user(user_service: UserServiceDep, user_data: UserCreate) -> User:
     user = await user_service.create_user(user_data)
     return user
 
 
-@router.get("/me", response_model=UserRead, status_code=200)
+@router.get("/me", response_model=UserRead)
 async def get_me(user: CurrentUserDep) -> User:
     return user
