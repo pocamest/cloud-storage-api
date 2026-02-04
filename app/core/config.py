586a2a332de1549fcdbe17c7de57aa7fc4eb2f9d
pocamest <pocamest@yandex.ru>
2026-02-05 00:59:31 +0300

    feat(storage): implement s3 client dependency

--- app/core/config.py
+++ app/core/config.py
@@ -17,6 +17,11 @@ class Settings(BaseSettings):
     redis_port: int = 6379
     redis_host: str = "localhost"
 
+    s3_access_key: str
+    s3_secret_key: str
+    s3_endpoint_url: str
+    s3_bucket_name: str
+
     log_level: str = "INFO"
 
     token_secret_key: str
--- app/core/dependencies.py
+++ app/core/dependencies.py
@@ -4,9 +4,11 @@ from typing import Annotated
 from fastapi import Depends
 from redis.asyncio import Redis
 from sqlalchemy.ext.asyncio import AsyncSession
+from types_aiobotocore_s3 import S3Client
 
 from app.core.database import session_factory
 from app.core.redis import redis_client
+from app.core.s3 import s3_provider
 
 
 async def get_session() -> AsyncGenerator[AsyncSession]:
@@ -22,3 +24,10 @@ async def get_redis_client() -> Redis:
 
 
 RedisDep = Annotated[Redis, Depends(get_redis_client)]
+
+
+async def get_s3_client() -> S3Client:
+    return s3_provider.client
+
+
+S3ClientDep = Annotated[S3Client, Depends(get_s3_client)]
